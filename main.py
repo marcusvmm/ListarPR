f40c37ec8019798659aad61989ffe6962d11a6ba
Marcusvmm <marcusmacielmonteiro@gmail.com>
2022-11-02 10:31:37 -0300

    UPDATE README

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 project = input('Insira o nome do projeto:')
 repo = input('Insira o nome do repositório:')
-pr_state = 'closed'
+pr_state = 'open'
 per_page = '10'
 result = requests.get('https://api.github.com/repos/'+ project +'/'+ repo +'/pulls?state=' + pr_state +'&per_page='+ per_page)
 json_result = result.json()
